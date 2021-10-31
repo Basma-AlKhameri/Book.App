5be858468617d0378ba6dd04c7a1f2bfccd5ed54
Basma-AlKhameri <basma21970@gmail.com>
2021-10-31 12:16:07 +0300

    fix navigation bottom

--- app/src/main/java/com/example/bookapp/Home_Page.kt
+++ app/src/main/java/com/example/bookapp/Home_Page.kt
@@ -9,6 +9,7 @@ import com.example.bookapp.DataClass.images
 import com.example.bookapp.adapters.Category_recyclerview_adapter
 import com.example.bookapp.fragment.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.bottomnavigation.BottomNavigationView.OnNavigationItemSelectedListener
 
 class Home_Page : AppCompatActivity() {
 
@@ -17,29 +18,39 @@ class Home_Page : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
 
-         val homeFragment = Home_icon_fragment()
+val bottomNavigationView2= findViewById<BottomNavigationView>(R.id.bottomNavigationView2)
+        val bottomNavigationView1= findViewById<BottomNavigationView>(R.id.bottomNavigationView1)
+
+        val homeFragment = Home_icon_fragment()
         val favotiteFragment=favorite_icon_fragment()
         val profileFragment= profile_icon_fragment()
         val searchFragment= search_icon_fragment()
         val cartFragment= cart_icon_fragment()
 
         setCurrentFragment(homeFragment)
+        bottomNavigationView1.setOnNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.isearch-> setCurrentFragment(searchFragment)
+                 R.id.icart-> setCurrentFragment(cartFragment)
+            }
+            true
+        }
 
-        BottomNavigationView.OnNavigationItemSelectedListener {
+        bottomNavigationView2.setOnNavigationItemSelectedListener {
             when(it.itemId) {
                 R.id.ihome -> setCurrentFragment(homeFragment)
                 R.id.ifavorite -> setCurrentFragment(favotiteFragment)
                 R.id.iprofile -> setCurrentFragment(profileFragment)
 
-                R.id.isearch-> setCurrentFragment(searchFragment)
-                R.id.icart-> setCurrentFragment(cartFragment)
+               // R.id.isearch-> setCurrentFragment(searchFragment)
+               // R.id.icart-> setCurrentFragment(cartFragment)
             }
             true
         }
 
     }
-private fun setCurrentFragment(fragment: Fragment)= supportFragmentManager.beginTransaction()
-    .apply { replace(R.id.flFragment, fragment)
+private fun setCurrentFragment(fragment: Fragment)=
+    supportFragmentManager.beginTransaction().apply { replace(R.id.flFragment, fragment)
         commit()
     }
 }
