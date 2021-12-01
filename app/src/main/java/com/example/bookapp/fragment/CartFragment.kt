082f021bcf1ffc112f8ab8996e8276274ca125d6
manal1440 <manalhd.1440@gmail.com>
2021-12-01 14:12:19 +0300

    step View

--- app/src/main/java/com/example/bookapp/fragment/CartFragment.kt
+++ app/src/main/java/com/example/bookapp/fragment/CartFragment.kt
@@ -1,11 +1,16 @@
 package com.example.bookapp.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
+import androidx.fragment.app.Fragment
 import com.example.bookapp.R
+import com.shuhart.stepview.StepView
+import kotlinx.android.synthetic.*
+
 
 class CartFragment : Fragment(R.layout.cart_fragment) {
 
@@ -13,7 +18,23 @@ class CartFragment : Fragment(R.layout.cart_fragment) {
         super.onCreate(savedInstanceState)
 
     }
+fun stepupStepupView(){
 
+    val stepView=View.FindViewById(R.id.step_view_cart)
+    stepView.State
+         // You should specify only stepsNumber or steps array of strings.
+        // In case you specify both steps array is chosen.
+        .steps(object : ArrayList<String?>() {
+            init {
+                add("First step")
+                add("Second step")
+                add("Third step")
+            }
+        })
+        .stepsNumber(3)
+        .animationDuration(resources.getInteger(android.R.integer.config_shortAnimTime))
+        .commit()
+}
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
